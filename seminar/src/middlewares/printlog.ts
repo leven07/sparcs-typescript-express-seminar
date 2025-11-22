59f7f8d9995ae963efae2731096171253b06fe04
leven07 <leven@sparcs.org>
2025-11-22 19:15:35 +0900

    fix(printlog.ts): fixed printing time

--- seminar/src/middlewares/printlog.ts
+++ seminar/src/middlewares/printlog.ts
@@ -2,6 +2,6 @@ import {Request, Response, NextFunction} from "express";
 
 export default async (req: Request, res: Response, next: NextFunction) => {
     const time = new Date()
-    console.log(`Method: ${req.method}, Path: ${req.path}, Time: ${time.toISOString}`);
+    console.log(`Method: ${req.method}, Path: ${req.path}, Time: ${time.toISOString()}`);
     next();
 }
